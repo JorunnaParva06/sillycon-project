7e99028d70a681a60df25cd72d8e2e865e694040
JorunnaParva06 <jedwua9@gmail.com>
2026-02-13 19:48:06 -0700

    modified main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from sklearn.model_selection import *
-from sklearn.tree import *
-from sklearn.metrics import *
-from sklearn.preprocessing import *
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import k_means
+from sklearn.metrics import adjusted_rand_score
+import pandas as pd
 
 print("Hello World!")
+
+# Get a list of all the messages
